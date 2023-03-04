9c85fcb3caa8169b4de3f59e21d230c357d7dc64
marco0019 <64751677+marco0019@users.noreply.github.com>
2023-03-04 14:56:21 +0100

    init

--- App.js
+++ App.js
@@ -1,20 +1,36 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Axios from 'axios';
+import { useReducer, useContext } from 'react/cjs/react.development';
 
+export const [user, setUser] = useContext("")
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    Axios.get(`https://eu-central-1.aws.webhooks.mongodb-realm.com/api/client/v2.0/app/data-kxsej/service/exampleApi/incoming_webhook/insert?secret=exampleApi`).then(function (response) { setUser(response.data); console.log(response.data); })
+        .catch(function (error) {
+            console.log(error);
+        });
+        console.log(user);
+    return (
+        <View style={styles.container}>
+            <Text style={styles.text}>{user}</Text>
+            <Text>Open up App.js to start working on your app!</Text>
+            <StatusBar style="auto" />
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    text: {
+        color: '#000'
+    }
 });
+/*
+
+
+*/
---
+++ Users.js
@@ -0,0 +1,16 @@
+/*import { View, Text, StyleSheet } from 'react-native'
+import Axios from 'axios';
+import { useState } from 'react'
+
+export default function Users() {
+    const [users, setUsers] = useState([{}]);
+    Axios.get("https://eu-central-1.aws.webhooks.mongodb-realm.com/api/client/v2.0/app/data-kxsej/service/exampleApi/incoming_webhook/getAll?secret=getAll&database=Users&collection=users").then(function (response) { setUsers(response.data); })
+        .catch(function (error) {
+            console.log(error);
+        });
+    return (
+        <View>
+            {users.map(usr=><Text key={usr}>{usr.user}</Text>)}
+        </View>
+    )
+}*/
